a86decc5503bb6fc56476d7ae964cd186a1d044f
Hattwick <philhattwick@charter.net>
2018-10-04 21:35:24 -0400

    App-randoms - start of collection of approaches

--- app-randoms.py
+++ app-randoms.py
@@ -1,9 +1,11 @@
-# Goofing around with Michael's "Build 10 Apps" tutorial at TalkPython Training
-
 # This is a mix of several use cases of the random function
 
 
 import random
+import sys
+
+version = sys.version
+print(version)
 
 
 # first pass just select a random element from a string.
@@ -12,5 +14,3 @@ item = random.choice(letters)
 print("Found random item: " + item)
 item = random.choice(letters)  #do it again
 print("Found random item: {0}".format(item))
-
-
---
+++ app3dates.py
@@ -0,0 +1,8 @@
+# Goofing around with Michael's "Build 10 Apps" tutorial at TalkPython Training
+
+import sys
+
+version = sys.version
+print(version)
+
+
