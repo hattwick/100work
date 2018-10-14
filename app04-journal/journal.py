08760bf6a6775e0018ebe97388d449c57f70066b
Hattwick <philhattwick@charter.net>
2018-10-14 14:25:28 -0400

    App4-journal-journal.py backstep to address file i/o errors

--- app04-journal/journal.py
+++ app04-journal/journal.py
@@ -9,9 +9,8 @@ This is the journal module
 import os
 
 
-def get_full_pathname(name):
+def get_full_pathname(name: object) -> object:
     """
-
     :rtype: object
     """
     filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
@@ -23,26 +22,30 @@ def load(name):
     Method to create and load new journal
     :param name: base of the journal to load
     :return: a new journal data structure with file data
-    """
-data = []
-    filename = get_full_pathname(name)
 
-    if os.path.exists(filename):
-        with open(filename) as fin:
-            for entry in fin.readlines():
-                print("loading: " + entry.rstrip())
+	data = []
+	filename = get_full_pathname(name)
 
-        return data
+
+if os.path.exists(filename):
+	with open(filename) as fin:
+		for entry in fin.readlines():
+			print("loading: " + entry.rstrip())
+
+	return data
+	"""
+    return []
 
 
 def save(name, journal_data):
-    filename = get_full_pathname(name)
-    print("saving to......... {}".format(filename))
-    # fout = open(filename, 'w')
-    with open(filename) as fout:
-        for entry in journal_data:
-            fout.write(entry + '\n')
+	filename = os.path.abspath(os.path.join('./journals/', name+ '.jrl'))
+	print("saving to......... {}".format(filename))
+
+	fout = open(filename, 'w')
+	# with open(filename) as fout:
+	for entry in journal_data:
+		fout.write(entry + '\n')
 
 
 def add_entry(text, journal_data):
-    journal_data.append(text)
+	journal_data.append(text)
