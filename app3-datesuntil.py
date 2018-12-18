9de69793fecf11d91a3e9eaf99bf88f6c3c313a7
Hattwick <philhattwick@charter.net>
2018-12-17 21:55:32 -0500

    black applied

--- app3-datesuntil.py
+++ app3-datesuntil.py
@@ -9,38 +9,37 @@ print(version)
 
 
 def program_header():
-	print('----------------------------------')
-	print('       DAYS UNTIL APP             ')
-	print('----------------------------------')
+    print("----------------------------------")
+    print("       DAYS UNTIL APP             ")
+    print("----------------------------------")
 
 
 def get_special_day_from_user():
-	print("What is the day you are tracking toward?")
-	year = int(input ("Year [YYYY] "))
-	month = int(input ("Month [MM] "))
-	day = int(input ("Day [DD] "))
-	targetday=datetime.date(year, month, day)
-	print(targetday)
-	return targetday
+    print("What is the day you are tracking toward?")
+    year = int(input("Year [YYYY] "))
+    month = int(input("Month [MM] "))
+    day = int(input("Day [DD] "))
+    targetday = datetime.date(year, month, day)
+    print(targetday)
+    return targetday
 
 
 def compute_days_between_dates(original_date, target_date):
-	dt = original_date - target_date
-	# print('days' + dt)
-	return dt.days
+    dt = original_date - target_date
+    # print('days' + dt)
+    return dt.days
 
 
 def findings():
-	pass
+    pass
 
 
 if __name__ == "__main__":
-	# execute only if run as a script
-	program_header()
-	day = get_special_day_from_user()
-	now = date.today()          #TODO fix zero value being passed
-	print(f'Today is {0}'.format(now))
-	number_of_days = compute_days_between_dates(day, now)
-	print(f'Number of days: '.format(number_of_days))
-	# findings(number_of_days)
-
+    # execute only if run as a script
+    program_header()
+    day = get_special_day_from_user()
+    now = date.today()  # TODO fix zero value being passed
+    print(f"Today is {0}".format(now))
+    number_of_days = compute_days_between_dates(day, now)
+    print(f"Number of days: ".format(number_of_days))
+    # findings(number_of_days)
--- app3dates.py
+++ app3dates.py
@@ -8,32 +8,31 @@ print(version)
 
 
 def program_header():
-	print('----------------------------------')
-	print('       DAYS UNTIL APP             ')
-	print('----------------------------------')
+    print("----------------------------------")
+    print("       DAYS UNTIL APP             ")
+    print("----------------------------------")
 
 
 def get_special_day_from_user():
-	print("What is the day you are tracking toward?")
-	year = input ("Year [YYYY}")
-	month = input ("Month [MM}")
-	day = input ("Day [DD}")
-	targetday=datetime.date
-	print(f'Target date locked in as'.format(targetday))
+    print("What is the day you are tracking toward?")
+    year = input("Year [YYYY}")
+    month = input("Month [MM}")
+    day = input("Day [DD}")
+    targetday = datetime.date
+    print(f"Target date locked in as".format(targetday))
 
 
 def compute_days_between_dates():
-	pass
+    pass
 
 
 def findings():
-	pass
+    pass
 
 
 def main():
-	program_header()
-	day = get_special_day_from_user()
-	now=None
-	number_of_days = compute_days_between_dates(day,now)
-	findings(number_of_days)
-
+    program_header()
+    day = get_special_day_from_user()
+    now = None
+    number_of_days = compute_days_between_dates(day, now)
+    findings(number_of_days)
