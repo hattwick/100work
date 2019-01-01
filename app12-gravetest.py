a41e7e9f4c979c4c0f119050363a2efb181665ca
Phil Hattwick <phil@hattwick.com>
2019-01-01 08:07:11 -0500

    added examples

--- app12-gravetest.py
+++ app12-gravetest.py
@@ -6,12 +6,18 @@ from networkx.algorithms.approximation.dominating_set import min_weighted_domina
 
 from grave import plot_network
 
-network = nx.powerlaw_cluster_graph(10, 2, .1)
+network = nx.powerlaw_cluster_graph(55, 1, .2)
 """
+Parameter examples:
 10,2,.1 almost bowtie
+50, 1, .2 Doc example - multi hub and spoke
+51,1,.2 two hubs start connecting
+52,1,.2 split universe
+53 or 54,1,.2 starburst with offshoots
+55,1,.2 interconnected clusters - path management essential
 52,2,.3 near-symmetrical constellation
-50, 12, .4 more symmetrical, dense web
 
+50, 12, .4 more symmetrical, dense web
 """
 dom_set = min_weighted_dominating_set(network)
 
@@ -22,7 +28,7 @@ def color_dominators(node_attrs):
     if node_attrs.get('is_dominator', False):
         return {'color': 'red'}
     else:
-        return {'color': 'black'}
+        return {'color': 'green'}
 
 fig, ax = plt.subplots()
 plot_network(network, node_style=color_dominators)
