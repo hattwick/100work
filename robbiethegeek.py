c76163c58e05484ab4824e45220212f175e4c811
phil <phil@hattwick.com>
2019-05-06 22:39:03 -0400

    initial

---
+++ robbiethegeek.py
@@ -0,0 +1,42 @@
+# Robbie posed a question.
+# A network guy is just one of the many things he is.
+# I needed time to prepare the answer
+
+import sys
+import collections
+
+version = sys.version
+print(version)
+
+
+def main():
+    program_header()
+    get_and_transform()
+
+
+def program_header():
+    print("--------------------------------------------")
+    print("           ROBBIE's CODE MUNGER             ")
+    print("--------------------------------------------")
+    print()
+
+
+
+
+def get_and_transform():
+    """
+    Get a value (e.g. aabbbbccdaaa) and return a shorthand(?)
+    version (e.g. 2a4b2c1d3a).
+    :return: is optional
+
+    TODO: error handling, loop capability
+    """
+
+    codedictionary = {}  #initialize dictionary of letters and count
+    robbieinput = input("Enter your string? ")
+    print(f"Original string: {robbieinput}")
+
+
+
+if __name__ == "__main__":
+    main()
