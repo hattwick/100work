7462b316a8a2ce12e4796fdd749335dfcdb81b10
phil <phil@hattwick.com>
2019-08-30 00:07:42 -0400

    added request loop

--- resolve1.py
+++ resolve1.py
@@ -1,7 +1,9 @@
 # Python 100 days - simple dns resolver
 # Tested in native 3.6.5 and 3.7.2.  Socket package not available in Anaconda
+# Added some requests activity
 
 import socket
+import requests
 
 class Resolver:
     """
@@ -21,3 +23,7 @@ if __name__ == "__main__":
     testresolve = Resolver()
     testresult = testresolve("google.com")
     print(testresult)
+    webcheck = requests.get('http://google.com')
+    print ('request get result: ', webcheck)
+    print ('Request response code: ', webcheck.status_code)
+    print ('Requested contents: ', webcheck.content)
